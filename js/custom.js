97f18bfb53fa3c7cabf215e66b62357257833537
wo0odychan <wo0ody@kakao.com>
2023-06-21 09:51:15 +0900

    캥거루쓰

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,60 @@
 $(function () {
 
+    $(window).on('scroll', function () {
+        const sct = $(window).scrollTop();
+        if (sct > 0) {
+            $('.Header').addClass('on')
+        }
+        else {
+            $('.Header').removeClass('on')
+        }
+    });
+
+    const MainSlide = new Swiper('.main_slide', {
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        on: {
+            slideChangeTransitionStart: function () {
+                $('.MainVisual .dots li')
+                    .eq(this.realIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on')
+            }
+        }
+    });
+
+    const ItmSlide = new Swiper('.itm_slide', {
+        loop: true,
+        slidesPerView: 2,
+        spaceBetween: 400,
+        speed: 900,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+    })
+
+
+    $('.MainVisual .arrows .left').on('click', function () {
+        MainSlide.slidePrev();
+    });
+    $('.MainVisual .arrows .right').on('click', function () {
+        MainSlide.slideNext();
+    });
+
+
+    $('.MainVisual .dots li').on('click', function () {
+        const idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on')
+        MainSlide.slideTo(idx);
+    })
+
+
+
+
+
 })
